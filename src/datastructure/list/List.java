63bebc8ff88a5b073b862750fc0477b279f383ca
TFOX203 <pesasimeran87@gmail.com>
2026-02-13 11:38:36 +0100

    list debug

--- src/datastructure/list/List.java
+++ src/datastructure/list/List.java
@@ -8,16 +8,46 @@ package datastructure.list;
 public class List<E> {
 
 	// The reference to the last element in the list
-	private Node last;
+	private Node<E> last;
 	// The reference to the first element of the list
-	private Node first;
+	private Node<E> first;
 
-	public void add(Node<E> node) {
-
-		last.next = node;
+	public void add(Node<E> node) throws Exception {
+		//1. check the parameter if it is valid
+		if(node == null) {
+			throw new Exception("You can not pass a null node to the list");
+			
+		}
+		//2. add the node to the tail of the list
+		//2.1 Check first if the head element is null
+		// in the case of null, it means the list is empty
+		if(first == null) {//if the list is empty
+			// the list is empty
+			last = node;
+			first = node;
+		}
+		else {
+			last.next = node;
+			last = node;
+			
+		}
+		
+		
 
 	}
 
+	public Node<E> getLast() {
+		return last;
+	}
+
+	public Node<E> getFirst() {
+		return first;
+	}
+
+		
+	public void printList() {
+		
+	}
 	
 }
 
@@ -25,8 +55,11 @@ public class List<E> {
 
 class Node<E> {
 
+	public Node(E e) {
+		data = e;
+	}
 	// The data stored in a node of the list
-	private E e;
+	private E data;
 	// Each node needs to pint to the next node, we use this variable to do it
 	protected Node<E> next;
 
